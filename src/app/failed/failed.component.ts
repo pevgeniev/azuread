a4c6d221773c23e554dbe2928fdeb2b49818b33b
Petar Tsvetanov <pevgeniev@gmail.com>
2025-10-07 09:20:31 +0300

    deploy , clean unused components

--- src/app/failed/failed.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'login-failed',
-  template: `<p>Login failed. Please contact the administrator.</p>`,
-  styleUrls: [],
-})
-export class FailedComponent {}
